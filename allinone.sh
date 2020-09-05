45ebfd5df3cb0e1df0eed417ce60b82c51ab55d2
AaronJaws <aaron.jaws@fir3cloud.net>
2020-09-05 09:42:38 +0800

    Update allinone.sh

--- allinone.sh
+++ allinone.sh
@@ -6,7 +6,7 @@ sh_ver="1.0.0"
 
 # - ESSENTIAL
 install_env() {
-    # Sources Update
+	# Sources Update
 	cd /etc/apt/
 	wget --no-check-certificate https://config.nliu.work/sources_d10.list
 	mv sources_d10.list sources.list
@@ -15,7 +15,7 @@ install_env() {
 	cd ~
 	apt install -y curl wget nano net-tools htop nload iperf3 screen ntpdate tzdata dnsutils mtr git rng-tools unzip zip tuned
 	# Setup rng-tools and tuned
-	echo "HRNGDEVICE=/dev/urandom" >> /etc/default/rng-tools
+	echo "HRNGDEVICE=/dev/urandom" >>/etc/default/rng-tools
 	systemctl enable rng-tools && systemctl restart rng-tools
 	systemctl enable tuned
 	tuned-adm profile virtual-guest
@@ -26,8 +26,8 @@ install_env() {
 	rm -f /etc/security/limits.conf
 	wget --no-check-certificate https://storage.acrtis.app/limits
 	wget --no-check-certificate https://storage.acrtis.app/sysctl
-	cat limits > /etc/security/limits.conf
-	cat sysctl >> /etc/sysctl.conf
+	cat limits >/etc/security/limits.conf
+	cat sysctl >>/etc/sysctl.conf
 	rm -rf limits sysctl
 }
 
@@ -42,44 +42,44 @@ Set_forwarding_port() {
 	[[ -z "${forwarding_port}" ]] && echo "Enter something when asking" && exit 1
 	echo && echo -e " forwarding ports: ${forwarding_port}" && echo
 }
-Set_forwarding_ip(){
+Set_forwarding_ip() {
 	read -e -p "type in the address you wanna forward to:" forwarding_ip
 	[[ -z "${forwarding_ip}" ]] && echo "Enter something when asking" && exit 1
 	echo && echo -e "forwarding IP: ${forwarding_ip}" && echo
 }
-Set_local_ip(){
+Set_local_ip() {
 	read -e -p "Type in your local IP address:" local_ip
 	[[ -z "${local_ip}" ]] && echo "Enter something when asking" && exit 1
 	echo && echo -e "local IP: ${local_ip}" && echo
 }
 
 Clear_iptables() {
-    iptables -P INPUT ACCEPT
+	iptables -P INPUT ACCEPT
 	iptables -P FORWARD ACCEPT
 	iptables -P OUTPUT ACCEPT
 	iptables -t nat -F
 	iptables -t mangle -F
 	iptables -F
 	iptables -X
-	iptables-save > /root/rules
+	iptables-save >/root/rules
 }
 Add_iptables() {
-    Set_forwarding_port
-    Set_forwarding_ip
-    Set_local_ip
-    iptables -t nat -A PREROUTING -p tcp --dport ${forwarding_port} -j DNAT --to-destination ${forwarding_ip}
-    iptables -t nat -A PREROUTING -p udp --dport ${forwarding_port} -j DNAT --to-destination ${forwarding_ip}
-    iptables -t nat -A POSTROUTING -p tcp -d ${forwarding_ip} --dport ${forwarding_port} -j SNAT --to-source ${local_ip}
-    iptables -t nat -A POSTROUTING -p udp -d ${forwarding_ip} --dport ${forwarding_port} -j SNAT --to-source ${local_ip}
-    iptables-save > /root/rules
+	Set_forwarding_port
+	Set_forwarding_ip
+	Set_local_ip
+	iptables -t nat -A PREROUTING -p tcp --dport ${forwarding_port} -j DNAT --to-destination ${forwarding_ip}
+	iptables -t nat -A PREROUTING -p udp --dport ${forwarding_port} -j DNAT --to-destination ${forwarding_ip}
+	iptables -t nat -A POSTROUTING -p tcp -d ${forwarding_ip} --dport ${forwarding_port} -j SNAT --to-source ${local_ip}
+	iptables -t nat -A POSTROUTING -p udp -d ${forwarding_ip} --dport ${forwarding_port} -j SNAT --to-source ${local_ip}
+	iptables-save >/root/rules
 }
-View_forwarding(){
-    iptables -nvL -t nat
+View_forwarding() {
+	iptables -nvL -t nat
 }
 
 # - SHIT INSTALLING
 kernel_upgrade() {
-   apt install -t buster-backports linux-image-cloud-amd64 linux-headers-cloud-amd64 -y
+	apt install -t buster-backports linux-image-cloud-amd64 linux-headers-cloud-amd64 -y
 }
 install_haproxy() {
 	# Install haproxy
@@ -90,17 +90,17 @@ install_haproxy() {
 	wget -O /etc/haproxy/haproxy.cfg https://storage.acrtis.app/haproxy
 }
 install_docker() {
-    # Docker Installation
+	# Docker Installation
 	curl -fsSL get.docker.com | bash
 	#curl -sSL https://get.daocloud.io/docker | sh
 }
 install_speedtest() {
-    apt-get install gnupg1 apt-transport-https dirmngr -y
-    apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61
-    echo "deb https://ookla.bintray.com/debian stretch main" | tee /etc/apt/sources.list.d/speedtest.list
-    apt update --allow-insecure-repositories
-    apt -y install speedtest
-} 
+	apt-get install gnupg1 apt-transport-https dirmngr -y
+	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61
+	echo "deb https://ookla.bintray.com/debian stretch main" | tee /etc/apt/sources.list.d/speedtest.list
+	apt update --allow-insecure-repositories
+	apt -y install speedtest
+}
 
 # Launchpad
 echo && echo -e "
@@ -125,34 +125,34 @@ echo && echo -e "
 " && echo
 read -e -p " Enter (0-7):" num
 case "$num" in
-    0)
-    install_env
+0)
+	install_env
 	;;
-	1)
-    check_iptables
+1)
+	check_iptables
 	Add_iptables
 	;;
-	2)
-    check_iptables
+2)
+	check_iptables
 	Clear_iptables
 	;;
-	3)
-    check_iptables
+3)
+	check_iptables
 	View_forwarding
 	;;
-    4)
-    kernel_upgrade
+4)
+	kernel_upgrade
 	;;
-    5)
-    install_haproxy
+5)
+	install_haproxy
 	;;
-    6)
-    install_docker
+6)
+	install_docker
 	;;
-    7)
-    install_speedtest
+7)
+	install_speedtest
 	;;
-	*)
+*)
 	echo "enter ONLY from 0-7 bruh"
 	;;
 esac
