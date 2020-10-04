910e78e1ab0d9c33b3abb6883dd1806d13dfec92
AaronJaws <aaron.jaws@fir3cloud.net>
2020-10-04 23:32:33 +0800

    Update allinone.sh

--- allinone.sh
+++ allinone.sh
@@ -27,7 +27,7 @@ install_env() {
     iptables-save >/root/rules
     echo '#!/bin/sh' >/etc/rc.local
     echo 'default_route=`ip route | grep "^default" | head -1`' >>/etc/rc.local
-    echo 'ip route change $default_route initcwnd 20' >>/etc/rc.local
+    echo 'ip route change $default_route initcwnd 15 initrwnd 15' >>/etc/rc.local
     echo 'iptables-restore < /root/rules' >>/etc/rc.local
     chmod +x /etc/rc.local
 }
